8be101915119591f155d75d504b1604b33f9b876
David Canavan <davidcanavan@live.com>
2022-04-02 17:56:57 +0100

    initial commit

---
+++ mp3tagger/src/main/java/mp3tagger/test.java
@@ -0,0 +1,14 @@
+package mp3tagger;
+
+import com.mpatric.mp3agic.Mp3File;
+
+public class test {
+
+	
+	private Mp3File file; 
+	
+	public static void main(String[] args) {
+		
+		System.out.println("test");
+	}
+}
